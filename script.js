8118877b8d1de5a829cdb5f79389bfc662193855
farfromnow <singasky97@gmail.com>
2018-12-20 11:33:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -155,3 +155,17 @@ class Application extends PIXI.Application {
 const app = new Application();
 app.load(manifest);
 
+  /* Demo purposes only */
+  $(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+
+$(".popup").click(function () {
+    var $this = $(this);
+    var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 400, "height": 300});
+    var $title = $("<h1>").text($this.data("title"));
+    $("#video-view").html($title).append($iframe);
+    $iframe.wrap("<div class='class-video'>");
+});
