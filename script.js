4d6926294080bdcb8e5d7dd202af63c3cdb3119b
farfromnow <singasky97@gmail.com>
2018-12-20 10:57:56 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -54,3 +54,104 @@ module.exports = {
     }
 };
     
+
+console.clear();
+
+const flameFrag = document.querySelector("#flame-frag").textContent;
+const baseUrl = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/";
+
+const manifest = [
+  { name: "noise", url: "noise-texture-11.png?v=9" }
+];
+
+
+//
+// FLAME FILTER
+// ===========================================================================
+class FlameFilter extends PIXI.Filter {
+  
+  constructor(texture, time = 0.0) {    
+    super(null, flameFrag);
+       
+    this.uniforms.dimensions = new Float32Array(2);   
+    this.texture = texture;
+    this.time = time;
+  }
+  
+  get texture() {
+    return this.uniforms.mapSampler;
+  }
+  
+  set texture(texture) {
+    texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+    this.uniforms.mapSampler = texture;
+  }
+  
+  apply(filterManager, input, output, clear) {
+          
+    this.uniforms.dimensions[0] = input.sourceFrame.width;
+    this.uniforms.dimensions[1] = input.sourceFrame.height;    
+    this.uniforms.time = this.time;
+
+    filterManager.applyFilter(this, input, output, clear);
+  }
+}
+
+
+//
+// APPLICATION
+// ===========================================================================
+class Application extends PIXI.Application {
+    
+  constructor() {
+    
+    if (window.devicePixelRatio > 1) {
+      PIXI.settings.RESOLUTION = 2;
+    }
+    
+    PIXI.settings.PRECISION_FRAGMENT = "highp";
+    
+    super({
+      view: document.querySelector("#view"),
+      width: window.innerWidth,
+      height: window.innerHeight,
+      backgroundColor: 0x000000,
+      autoResize: true
+    });   
+    
+    this.isResized = true;    
+    this.loader.baseUrl = baseUrl;   
+  }
+    
+  load(manifest) {
+    
+    this.loader
+      .add(manifest)
+      .load((l, r) => this.init(r));   
+  }
+    
+  init(resources) {     
+        
+    this.flame = new FlameFilter(resources.noise.texture);
+       
+    this.stage.filterArea = this.screen;
+    this.stage.filters = [this.flame];
+       
+    this.ticker.add(this.update, this);                     
+    window.addEventListener("resize", () => this.isResized = true);
+  }
+   
+  update(delta) {
+    
+    if (this.isResized) {
+      this.renderer.resize(window.innerWidth, window.innerHeight);
+      this.isResized = false;
+    }
+    
+    this.flame.time += 0.1 * delta;
+  }
+}
+
+const app = new Application();
+app.load(manifest);
+
